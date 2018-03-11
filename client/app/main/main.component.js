815e7d30e28314028c51c03fd289f4213df2f568
Ian <kirkas@Ians-MacBook-Pro.local>
2018-03-11 19:23:28 -0400

    Sorted out the bouty cards initially

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -20,7 +20,47 @@ export class MainController {
 		this.$http.get('/api/things')
 			.then(response => {
 				this.$log.debug("Wowzers!!", response);
+				// [{
+				// 	user: {
+				// 		name: "Kirkas",
+				// 		karma: 5
+				// 	},
+				// 	reward: {
+				// 		name: "Artisan's Memory",
+				// 		quantity: 10,
+				// 	},
+				// 	dateCreated: new Date(date.getFullYear(), date.getMonth(), 1)
+				// }, {
+				// 	user: {
+				// 		name: "Finnius",
+				// 		karma: 4.5
+				// 	},
+				// 	reward: {
+				// 		name: "Pearls",
+				// 		quantity: 1000
+				// 	},
+				// 	dateCreated: new Date(2017, date.getMonth(), 1)
+				// }, {
+				// 	user: {
+				// 		name: "Winston",
+				// 		karma: 2
+				// 	},
+				// 	reward: {
+				// 		name: "Pearls",
+				// 		quantity: 10000
+				// 	},
+				// 	dateCreated: new Date()
+				// }]
 				this.bountyCards = response.data;
+
+				for(var i = 0; i < this.bountyCards.length; i++) {
+					var card = this.bountyCards[i];
+
+					 // Checks to see if there is a .5 on our karma to make the display easier
+					if(card.user.karma % 1 !== 0) {
+						card.user.halfKarma = true;
+					}
+				}
 				this.loading = false;
 			});
 	}
@@ -32,6 +72,11 @@ export class MainController {
 			});
 			this.newThing = '';
 		}
+	}
+
+	getArrayOfLength = function(n) {
+		this.$log.debug("Here");
+		return new Array(Math.floor(n));
 	}
 
 	deleteThing(thing) {
